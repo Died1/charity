ec529ff4a3cf9878620070e6411f2f075cb0b0c4
Eddy F_____ <eddycampos2011@gmail.com>
2022-05-12 14:11:22 -0400

    conectando db en servidor

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "charity";
+$servername = "109.106.246.201";
+$username = "u224905841_charity";
+$password = "Edd&3035";
+$dbname = "u224905841_charity";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
