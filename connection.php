5b2b5629e9a07e188c7423222f7354d2a4d88e62
Eddy F_____ <eddycampos2011@gmail.com>
2022-05-12 14:16:25 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('Asia/Kolkata');
 $servername = "109.106.246.201";
 $username = "u224905841_charity";
-$password = "Edd&3035";
+$password = "Eddy3035";
 $dbname = "u224905841_charity";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
